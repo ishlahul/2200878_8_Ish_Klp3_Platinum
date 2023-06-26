8196f2021d857631804714a652dcba3f7ebb3e90
Idris Madyasto <idrismadyasto@gmail.com>
2023-06-26 17:58:47 +0700

    Mobile Automation - login

--- 3. Katalon Studio Android Automation Testing/Mobile_Secondhand/Scripts/Pages/Login/Popup Wrong Email or Password/Script1687772182022.groovy
+++ 3. Katalon Studio Android Automation Testing/Mobile_Secondhand/Scripts/Pages/Login/Popup Wrong Email or Password/Script1687772182022.groovy
@@ -16,17 +16,13 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 
-Mobile.callTestCase(findTestCase('Pages/Home Before Login/Tap Icon Akun'), [:], FailureHandling.STOP_ON_FAILURE)
+Mobile.waitForElementPresent(findTestObject('Page_Login/verify_Email.atau.kata.sandi.salah'), 0)
 
-Mobile.callTestCase(findTestCase('Pages/Akun Saya Before Login/Tap Button Masuk At Akun Saya Page'), [:], FailureHandling.STOP_ON_FAILURE)
+errorText = Mobile.getText(findTestObject('Page_Login/verify_Email.atau.kata.sandi.salah'), 0)
 
-Mobile.callTestCase(findTestCase('Pages/Login/Input Email'), [('email') : 'invalidemail@gmail.com'], FailureHandling.STOP_ON_FAILURE)
+KeywordUtil.logInfo('Popup Warning Message : ' + errorText)
 
-Mobile.callTestCase(findTestCase('Pages/Login/Input Password'), [('password') : 'invalidpass'], FailureHandling.STOP_ON_FAILURE)
-
-Mobile.callTestCase(findTestCase('Pages/Login/Tap Button Masuk'), [:], FailureHandling.STOP_ON_FAILURE)
-
-WebUI.callTestCase(findTestCase('Pages/Login/Popup Wrong Email or Password'), [('expected') : 'Email atau kata sandi salah'], 
-    FailureHandling.STOP_ON_FAILURE)
+Mobile.verifyMatch(errorText, expected, true)
 
