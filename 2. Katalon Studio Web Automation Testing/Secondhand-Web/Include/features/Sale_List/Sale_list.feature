76545083c97b741c5c1ad41f1f12e4ac5c446d55
ishlahul <i.afif37@gmail.com>
2023-06-29 19:24:03 +0700

    sale list web

---
+++ 2. Katalon Studio Web Automation Testing/Secondhand-Web/Include/features/Sale_List/Sale_list.feature
@@ -0,0 +1,35 @@
+#Author: your.email@your.domain.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+@SaleList
+Feature: Sale List
+  User wants to see sale list
+
+  @SellingList
+  Scenario Outline: User wants to access sale list
+    Given user go to login page
+    When user fill email <email> and password <password>
+		Then user click login button
+		And go to product page
+		And user click wish list navbar
+		And user click sold list navbar
+		Then user has verified at product page
+		
+    Examples: 
+      | email  						| password 	|
+			|	i.afif@gmail.com	| 123				|
---
+++ 2. Katalon Studio Web Automation Testing/Secondhand-Web/Object Repository/Page_Sale_List/verify_user_container.rs
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>verify_user_container</name>
+   <tag></tag>
+   <elementGuidId>b26a2917-81c5-4cef-8a03-22d704cf16f2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Daftar Jual Saya'])[1]/following::div[1]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.card.py-2.px-4.rounded-4.border-4.border-light.mt-5.d-flex.justify-content-between</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>7c91fa9c-816c-40c4-a009-1b4ccd37bf36</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card py-2 px-4 rounded-4 border-4 border-light mt-5 d-flex justify-content-between</value>
+      <webElementGuid>760210a9-e45b-4044-8900-826514937171</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+        
+
+      
+        afif
+        Solo
+      
+
+      Edit
+    
+  </value>
+      <webElementGuid>d142f032-4c66-4b4c-ba9e-b5b83e8e75b6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;pt-5 mt-5&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;card py-2 px-4 rounded-4 border-4 border-light mt-5 d-flex justify-content-between&quot;]</value>
+      <webElementGuid>f4f33b8b-b707-428d-8b08-a4f59d031d21</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Daftar Jual Saya'])[1]/following::div[1]</value>
+      <webElementGuid>a0c69477-d684-4b88-b70e-026e52926b27</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section/div/div</value>
+      <webElementGuid>51492e04-3daa-44c4-956f-dfb81abae0cc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+    
+        
+
+      
+        afif
+        Solo
+      
+
+      Edit
+    
+  ' or . = '
+    
+        
+
+      
+        afif
+        Solo
+      
+
+      Edit
+    
+  ')]</value>
+      <webElementGuid>06437c0d-db31-4431-9b3f-c3c40436e06d</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
